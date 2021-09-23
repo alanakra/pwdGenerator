9ab8f4a63be0c77e6631b2e4ca5941cd75cc6a0a
Alan <alanakra@hotmail.fr>
2021-09-23 14:08:55 +0200

    Show result

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Generator from "./components/generator";
 import { useState } from "react";
 
 function App() {
@@ -13,20 +12,29 @@ function App() {
       pwd += bigString.charAt(Math.random() * bigString.length)
     }
 
-    console.log(pwd) 
+    setOpenSentence(true)
+
+    setResult(pwd)
   }
 
   const [pwdLength, setPwdLength] = useState(8)
+  const [openSentence, setOpenSentence] = useState(false)
+  const [result, setResult] = useState("")
 
   return (
     <div>
       <h1 className="text-center text-3xl font-semibold text-gray-700">Password Generator</h1>
         <form className="text-center text-xl mt-8" onSubmit={generatePassword}>
-          <h2>Please enter the length of your password:</h2>
-          <input type="number" className="border-2 border-gray-700 mt-2" value={pwdLength} onChange={e => setPwdLength(e.target.value)}></input>
+          <h2>Please enter the length of your password (min 8 characters):</h2>
+          <input type="number" className="border-2 border-gray-700 mt-2" value={pwdLength} onChange={e => setPwdLength(e.target.value)} min="8" ></input>
           <button type="submit" className="ml-4 bg-green-400 p-2 rounded">Generate</button>
         </form>
-      <Generator/>
+
+        { openSentence
+          ? <p className="text-center text-xl mt-8">Your password is: <span className="font-bold">{result}</span></p>
+          : <></>
+        }
+          
     </div>
   );
 }
